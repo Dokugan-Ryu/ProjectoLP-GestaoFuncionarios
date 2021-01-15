1c67a7de2e4609db73f14803b09be1d66509fdc0
Dokugan-Ryu <77363951+Dokugan-Ryu@users.noreply.github.com>
2021-01-15 07:36:35 +0000

    kinda working

--- salarios.c
+++ salarios.c
@@ -20,7 +20,7 @@ void expandirSalarios(Salario *salarios, int *tam_salarios) {
 int lerFicheiroMes(Salario *salarios, int *tam_salarios, int *cont_salarios) {
     int num_entradas_lidas = 0;
 
-    char nome_ficheiro[50]="fev2021.txt";
+    char nome_ficheiro[50] = "fev2021.txt";
     /*lerString(nome_ficheiro, 50, "Nome do ficheiro a ler: ");*/
 
     FILE *fp = fopen(nome_ficheiro, "r");
@@ -112,119 +112,122 @@ void processarSalarios(
     obterInt(1900, 2021, "Ano (1900-2021): ");
 
     int entradas_lidas = lerFicheiroMes(salarios, tam_salarios, cont_salarios);
-    printf("---%d---", salarios[(*salarios_processados) +0].codigo);
+    printf("---%d---", salarios[(*salarios_processados) + 0].codigo);
     if (entradas_lidas != 0) {
         for (int i = 0; i < entradas_lidas; i++) {
-            
+
             int codigo = procurarFuncionario(
                     funcionarios,
                     salarios[(*salarios_processados) + i].codigo,
                     cont_funcionarios);
-
-            float idade = (((12 - funcionarios[codigo].nascimento.mes) + mes) / 12) + ano - (funcionarios[codigo].nascimento.ano + 1);
-            float antiguidade = (((12 - funcionarios[codigo].contratado.mes) + mes) / 12) + ano - (funcionarios[codigo].contratado.ano + 1);
-
-
-            salarios[(*salarios_processados) + i].b_idade = funcionarios[codigo].b_idade * idade;
-            salarios[(*salarios_processados) + i].b_antiguidade = funcionarios[codigo].b_antiguidade * antiguidade;
-            if (salarios[(*salarios_processados) + i].faltas == 0) {
-                salarios[(*salarios_processados) + i].b_assiduidade = funcionarios[codigo].b_antiguidade;
-            } else {
-                salarios[(*salarios_processados) + i].b_assiduidade = 0;
-            }
+            printf("*********** %d *********", codigo);
+            if (codigo != -1) {
+                float idade = (((12 - funcionarios[codigo].nascimento.mes) + mes) / 12) + ano - (funcionarios[codigo].nascimento.ano + 1);
+                float antiguidade = (((12 - funcionarios[codigo].contratado.mes) + mes) / 12) + ano - (funcionarios[codigo].contratado.ano + 1);
 
 
-            float salario_base = funcionarios[codigo].salario_hora * (
-                    NUM_HORAS_TRABALHO_DIA * salarios[(*salarios_processados) + i].dias +
-                    NUM_HORAS_TRABALHO_DIA * 0.5 * salarios[(*salarios_processados) + i].meios_dias +
-                    NUM_HORAS_TRABALHO_DIA * 1.5 * salarios[(*salarios_processados) + i].fds);
-
-            salarios[(*salarios_processados) + i].salario_bruto = salario_base * (
-                    1 +
-                    salarios[(*salarios_processados) + i].b_idade +
-                    salarios[(*salarios_processados) + i].b_antiguidade +
-                    salarios[(*salarios_processados) + i].b_assiduidade);
-
-            int taxa_irs;
-            int categoria_irs;
-            switch (funcionarios[codigo].estado_civil) {
-                case 0:
-                    categoria_irs = 0;
-                    break;
-                case 1:
-                    categoria_irs = 1;
-                    break;
-                default:
-                    categoria_irs = 2;
-            }
-            
-            for (int i = 0; i < 36; i++) {
-                if (salarios[(*salarios_processados) + i].salario_bruto <= irs[i].salario) {
-                    switch (funcionarios[codigo].num_filhos) {
-                        case 0:
-                            taxa_irs = irs[i].zero_filhos[categoria_irs];
-                            break;
-                        case 1:
-                            taxa_irs = irs[i].um_filhos[categoria_irs];
-                            break;
-                        case 2:
-                            taxa_irs = irs[i].dois_filhos[categoria_irs];
-                            break;
-                        case 3:
-                            taxa_irs = irs[i].tres_filhos[categoria_irs];
-                            break;
-                        case 4:
-                            taxa_irs = irs[i].quatro_filhos[categoria_irs];
-                            break;
-                        default:
-                            taxa_irs = irs[i].cincomais_filhos[categoria_irs];
-                    }
+                salarios[(*salarios_processados) + i].b_idade = funcionarios[codigo].b_idade * idade;
+                salarios[(*salarios_processados) + i].b_antiguidade = funcionarios[codigo].b_antiguidade * antiguidade;
+                if (salarios[(*salarios_processados) + i].faltas == 0) {
+                    salarios[(*salarios_processados) + i].b_assiduidade = funcionarios[codigo].b_antiguidade;
                 } else {
-                    taxa_irs = irs[36].zero_filhos[categoria_irs];
+                    salarios[(*salarios_processados) + i].b_assiduidade = 0;
                 }
-            }
 
-            salarios[(*salarios_processados) + i].taxa_irs =
-                    salarios[(*salarios_processados) + i].salario_bruto * (1 - taxa_irs);
-            
-            /*float taxa_ss_func, taxa_ss_empresa;
-            switch (funcionarios[codigo].cargo) {
-                case 0:
-                    taxa_ss_empresa = seg_soc[0].taxa_empresa;
-                    break;
-                case 1:
-                    int categoria_irs = 1;
-                    break;
-                default:
-                    int categoria_irs = 2;
-            }*/
-            
-            float taxa_ss_empresa = seg_soc[funcionarios[codigo].cargo].taxa_empresa;
-            float taxa_ss_func = seg_soc[funcionarios[codigo].cargo].taxa_trabalhador;
-            
-            salarios[(*salarios_processados) + i].taxa_ss_empresa =
-                    salarios[(*salarios_processados) + i].salario_bruto * (1 - taxa_ss_empresa);
-            
-            salarios[(*salarios_processados) + i].taxa_ss_funcionario =
-                    salarios[(*salarios_processados) + i].salario_bruto * (1 - taxa_ss_func);
-            
-            salarios[(*salarios_processados) + i].salario_liquido =
-                    salarios[(*salarios_processados) + i].salario_bruto -
-                    salarios[(*salarios_processados) + i].taxa_irs - 
-                    salarios[(*salarios_processados) + i].taxa_ss_funcionario +
-                    funcionarios[codigo].subs_alimentacao * (
+
+                float salario_base = funcionarios[codigo].salario_hora * (
+                        NUM_HORAS_TRABALHO_DIA * salarios[(*salarios_processados) + i].dias +
+                        NUM_HORAS_TRABALHO_DIA * 0.5 * salarios[(*salarios_processados) + i].meios_dias +
+                        NUM_HORAS_TRABALHO_DIA * 1.5 * salarios[(*salarios_processados) + i].fds);
+
+                salarios[(*salarios_processados) + i].salario_bruto = salario_base * (
+                        1 +
+                        salarios[(*salarios_processados) + i].b_idade +
+                        salarios[(*salarios_processados) + i].b_antiguidade +
+                        salarios[(*salarios_processados) + i].b_assiduidade);
+
+                int taxa_irs;
+                int categoria_irs;
+                switch (funcionarios[codigo].estado_civil) {
+                    case 0:
+                        categoria_irs = 0;
+                        break;
+                    case 1:
+                        categoria_irs = 1;
+                        break;
+                    default:
+                        categoria_irs = 2;
+                }
+
+                for (int i = 0; i < 36; i++) {
+                    if (salarios[(*salarios_processados) + i].salario_bruto <= irs[i].salario) {
+                        switch (funcionarios[codigo].num_filhos) {
+                            case 0:
+                                taxa_irs = irs[i].zero_filhos[categoria_irs];
+                                break;
+                            case 1:
+                                taxa_irs = irs[i].um_filhos[categoria_irs];
+                                break;
+                            case 2:
+                                taxa_irs = irs[i].dois_filhos[categoria_irs];
+                                break;
+                            case 3:
+                                taxa_irs = irs[i].tres_filhos[categoria_irs];
+                                break;
+                            case 4:
+                                taxa_irs = irs[i].quatro_filhos[categoria_irs];
+                                break;
+                            default:
+                                taxa_irs = irs[i].cincomais_filhos[categoria_irs];
+                        }
+                    } else {
+                        taxa_irs = irs[36].zero_filhos[categoria_irs];
+                    }
+                }
+
+                salarios[(*salarios_processados) + i].taxa_irs =
+                        salarios[(*salarios_processados) + i].salario_bruto * (1 - taxa_irs);
+
+                /*float taxa_ss_func, taxa_ss_empresa;
+                switch (funcionarios[codigo].cargo) {
+                    case 0:
+                        taxa_ss_empresa = seg_soc[0].taxa_empresa;
+                        break;
+                    case 1:
+                        int categoria_irs = 1;
+                        break;
+                    default:
+                        int categoria_irs = 2;
+                }*/
+
+                float taxa_ss_empresa = seg_soc[funcionarios[codigo].cargo].taxa_empresa;
+                float taxa_ss_func = seg_soc[funcionarios[codigo].cargo].taxa_trabalhador;
+
+                salarios[(*salarios_processados) + i].taxa_ss_empresa =
+                        salarios[(*salarios_processados) + i].salario_bruto * (1 - taxa_ss_empresa);
+
+                salarios[(*salarios_processados) + i].taxa_ss_funcionario =
+                        salarios[(*salarios_processados) + i].salario_bruto * (1 - taxa_ss_func);
+
+                salarios[(*salarios_processados) + i].salario_liquido =
+                        salarios[(*salarios_processados) + i].salario_bruto -
+                        salarios[(*salarios_processados) + i].taxa_irs -
+                        salarios[(*salarios_processados) + i].taxa_ss_funcionario +
+                        funcionarios[codigo].subs_alimentacao * (
                         salarios[(*salarios_processados) + i].dias + salarios[(*salarios_processados) + i].fds);
-                    
-            printf("\n----- Salário do funcionário código %d processado -----\n", funcionarios[codigo].codigo);
-            printf("Salário bruto: €%f\n", salarios[(*salarios_processados) + i].salario_bruto);
-            printf("Subsídio alimentação: €%f\n", salarios[(*salarios_processados) + i].salario_bruto);
-            printf("Salário líquido: €%f\n", salarios[(*salarios_processados) + i].salario_liquido);
-            printf("Retenção IRS: €%f\n", salarios[(*salarios_processados) + i].taxa_irs);
-            printf("Contribuição SegSoc do trabalhador: €%f\n", salarios[(*salarios_processados) + i].taxa_ss_funcionario);
-            printf("Contribuição SegSoc da entidade empregadora: €%f\n", salarios[(*salarios_processados) + i].taxa_ss_empresa);
-            
-            
 
+                printf("\n----- Salário do funcionário código %d processado -----\n", funcionarios[codigo].codigo);
+                printf("Salário bruto: €%f\n", salarios[(*salarios_processados) + i].salario_bruto);
+                printf("Subsídio alimentação: €%f\n", salarios[(*salarios_processados) + i].salario_bruto);
+                printf("Salário líquido: €%f\n", salarios[(*salarios_processados) + i].salario_liquido);
+                printf("Retenção IRS: €%f\n", salarios[(*salarios_processados) + i].taxa_irs);
+                printf("Contribuição SegSoc do trabalhador: €%f\n", salarios[(*salarios_processados) + i].taxa_ss_funcionario);
+                printf("Contribuição SegSoc da entidade empregadora: €%f\n", salarios[(*salarios_processados) + i].taxa_ss_empresa);
+
+
+            } else {
+                printf("\nFuncionário nao encontrado, ignorando entrada...\n");
+            }
         }
     }
 
